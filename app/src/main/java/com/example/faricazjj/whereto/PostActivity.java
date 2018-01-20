cfc2b6f92e64cee5548b994751a7cdedaa0afcc0
tiffanyc1107 <p938196@gmail.com>
2018-01-20 12:51:45 -0800

    added messages and notifications button to main

--- app/src/main/java/com/example/faricazjj/whereto/PostActivity.java
+++ app/src/main/java/com/example/faricazjj/whereto/PostActivity.java
@@ -50,18 +50,10 @@ public class PostActivity extends AppCompatActivity {
     public void addItemsOnMonth(){
         month = (Spinner) findViewById(R.id.month);
         List<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        list.add(11);
-        list.add(12);
+        for (int i = 1; i <=12; i++){
+            list.add(i);
+        }
+
         ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
